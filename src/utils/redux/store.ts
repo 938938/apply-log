f86db3aa026dd0b736af2647b106266ba90098ea
938938 <938kjh@gmail.com>
2026-02-05 22:16:08 +0900

    [FEAT] slice 적용

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import applicationReducer from './applicationSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    application: applicationReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
